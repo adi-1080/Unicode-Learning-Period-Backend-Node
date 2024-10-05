e096fc516c4b95b0cc45f7191976e0827401307a
adi-1080 <adityagupta5277@gmail.com>
2024-10-06 01:02:02 +0530

    Related recruiter with company

---
+++ models/recruiter-model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const recruiterSchema = new Schema({
+    name:{
+        type: String
+    },
+    age:{
+        type: Number
+    },
+    gender:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+    password:{
+        type: String
+    },
+    join_date:{
+        type: Date
+    },
+    qualification:{
+        type: [String]
+    },
+    current_position:{
+        type: String
+    },
+    salary:{
+        type: Number
+    },
+    company_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company'
+    }
+})
+
+const Recruiter = mongoose.model('Recruiter',recruiterSchema)
+
+export default Recruiter
