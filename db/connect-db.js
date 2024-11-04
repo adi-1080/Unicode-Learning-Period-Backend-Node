c52aa65a9bb064525f155aeec61eb57507b34267
adi-1080 <adityagupta5277@gmail.com>
2024-11-05 02:16:41 +0530

    Fixed deployment issues

--- db/connect-db.js
+++ db/connect-db.js
@@ -3,20 +3,30 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const connectDB = () => {
-    // CONNECTING TO MONGODB ATLAS USING MONGOOSE
-    mongoose.connect(`mongodb+srv://${process.env.MONGODB_ATLAS_UNICODE_USERNAME}:${process.env.MONGODB_ATLAS_UNICODE_PASSWORD}@${process.env.UNICODE_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.UNICODE_CLUSTER_NAME}`)
+const connectDB = async() => {
+    try{
+        // CONNECTING TO MONGODB ATLAS USING MONGOOSE
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_ATLAS_UNICODE_USERNAME}:${process.env.MONGODB_ATLAS_UNICODE_PASSWORD}@${process.env.UNICODE_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.UNICODE_CLUSTER_NAME}`,{
+                useNewUrlParser:true,
+                useUnifiedTopology:true
+            }
+        )
 
-    const db = mongoose.connection
+        const db = mongoose.connection
 
-    // FUNCTIONS TO EXECUTE ON CONNECTING WITH ATLAS
-    db.on('error', (err) => {
-        console.log(err);
-    })
+        // FUNCTIONS TO EXECUTE ON CONNECTING WITH ATLAS
+        db.on('error', (err) => {
+            console.log(err);
+        })
 
-    db.once('open', () => {
-        console.log('Database Connection Established');
-    })
+        db.once('open', () => {
+            console.log('Database Connection Established');
+        })
+    }
+    catch(err){
+        console.log("Failed to connect to database:",error);
+    }
+    
 }
 
 export default connectDB
